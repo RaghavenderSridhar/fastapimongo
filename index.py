3f07882e631de3c155bd6fc1d1fa5f5e55f91688
raghavendersridhar <raghav.ajay3@gmail.com>
2022-01-14 22:45:09 +0530

    Added cors middleware for react app

--- index.py
+++ index.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 from routes.student import student_router
+from fastapi.middleware.cors import CORSMiddleware
 
+client_apps =['http://localhost:3000'] #react app ip port
 app = FastAPI()
 app.include_router(student_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = client_apps,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+
+)
